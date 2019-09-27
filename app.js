57b7c126241d58bc8810b4ef0e44a329ca28f3f0
Aleksandar Vasilevski <vasilevski85@gmail.com>
2019-09-27 14:25:40 +0200

    updated cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 require("dotenv").config();
 const subscribeRouter = require('./routes/subscribe');
@@ -18,7 +18,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
@@ -26,17 +26,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-	res.header('Access-Contol-Allow-Origin', '*');
-	res.header('Access-Contol-Allow-Headers',
-	 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	 if(req.method === 'OPTIONS'){
-		 res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-		 return res.status(200).json({});
-	 }
-	 next();
-});
 
 app.use('/subscribe', subscribeRouter);
 
